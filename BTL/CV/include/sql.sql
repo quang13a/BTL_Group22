0387e85117ae39ba1380f619df24772101196560
nts6920 <you@example.com>
2021-01-18 05:07:22 +0700

    first commit

---
+++ BTL/CV/include/sql.sql
@@ -0,0 +1,341 @@
+
+
+-- Database: `CV`
+--
+
+--
+-- Table structure for table `aboutus_setup`
+--
+
+CREATE TABLE `aboutus_setup` (
+  `id` int(11) NOT NULL,
+  `shortdesc` text NOT NULL,
+  `heading` text NOT NULL,
+  `subheading` text NOT NULL,
+  `longdesc` text NOT NULL,
+  `website` varchar(250) NOT NULL,
+  `dob` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `aboutus_setup`
+--
+
+INSERT INTO `aboutus_setup` (`id`, `shortdesc`, `heading`, `subheading`, `longdesc`, `website`, `dob`) VALUES
+(1, 'Đôi khi sự đấu tranh luôn cần phải có trong cuộc sống. Nếu cuộc sống trôi qua thật suôn sẻ, chúng ta sẽ không hiểu được cuộc sống, không có được bản lĩnh, nghị lực như chúng ta cần phải có, cuộc sống thật công bằng, không phải vô cớ mà mọi điều xảy đến với ta',
+ 'UI/UX Designer & Web Developer.', 'Có ảnh là sửa','Thời gian là miễn phí nhưng nó vô giá. Bạn không thể sở hữu nó, nhưng bạn có thể sử dụng nó. Bạn có thể dùng nó, nhưng bạn không thể giữ nó. Một khi bạn làm mất nó, bạn sẽ không thể nào có lại được nó.',
+ 'https://www.kocoweb.com','06 Sept, 2000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_users`
+--
+
+CREATE TABLE `admin_users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `user_id` varchar(250) NOT NULL,
+  `user_pass` varchar(250) NOT NULL,
+  `user_access` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin_users`
+--
+
+INSERT INTO `admin_users` (`id`, `username`, `user_id`, `user_pass`, `user_access`) VALUES
+(1, 'Nguyễn Thanh Sơn', 'nts6920@admin.com', 'nts06092000', 1);
+INSERT INTO `admin_users` (`id`, `username`, `user_id`, `user_pass`, `user_access`) VALUES
+(2, 'Nguyễn Văn Quang', 'quang13@admin.com', 'quang136', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic_setup`
+--
+
+CREATE TABLE `basic_setup` (
+  `id` int(11) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `description` text NOT NULL,
+  `keyword` text NOT NULL,
+  `icon` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `basic_setup`
+--
+
+INSERT INTO `basic_setup` (`id`, `title`, `description`, `keyword`, `icon`) VALUES
+(1, 'Group_22', 'Tôi là: web developer.', 'Web development,PHP development.', '6658064271579517834-128.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `cname` varchar(250) NOT NULL,
+  `cemail` varchar(250) NOT NULL,
+  `csubject` text NOT NULL,
+  `cmessage` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `cname`, `cemail`, `csubject`, `cmessage`) VALUES
+(1, 'Nguyễn Tuấn', 'tuan45@gmail.com', 'Bạn cùng  lớp', 'Khi nào gặp cùng công ty.:))'),
+(2, 'Mai Trang', 'trangxinh23@gmail.com', 'Bạn thân ', 'Dạy tao CNTT đi mày');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_setup`
+--
+
+CREATE TABLE `personal_setup` (
+  `id` int(11) NOT NULL,
+  `profilepic` varchar(250) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `facebook` varchar(200) NOT NULL,
+  `instagram` varchar(200) NOT NULL,
+  `github` varchar(200) NOT NULL,
+  `homewallpaper` varchar(200) NOT NULL,
+  `professions` varchar(200) NOT NULL,
+  `location` text NOT NULL,
+  `mobile` varchar(200) NOT NULL,
+  `emailid` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `personal_setup`
+--
+
+INSERT INTO `personal_setup` (`id`, `profilepic`, `name`, `facebook`, `instagram`,`github`, `homewallpaper`, `professions`, `location`, `mobile`, `emailid`) VALUES
+(1, '118593402_780715232695126_7151664726896327746_n.jpg', 'Sơn', '', 'https://www.facebook.com/nguyenthanhson6920/', 'https://www.instagram.com/nts.6920/', 'https://github.com/nts6920','50931745_2006720012961188_5312821008384655360_n.jpg','Web Developer,PHP Developer,Youtuber,Graphic Designer',
+'Số 6,ngõ 140 Trần Phú,Phường Mỗ  Lao,Quận Hà Đông','0356342770','nts6920@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `portfolio`
+--
+
+CREATE TABLE `portfolio` (
+  `id` int(11) NOT NULL,
+  `projectname` varchar(250) NOT NULL,
+  `projectpic` varchar(250) NOT NULL,
+  `projectlink` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `portfolio`
+--
+
+INSERT INTO `portfolio` (`id`, `projectname`, `projectpic`, `projectlink`) VALUES
+(1, 'Bóng đá', '60805324_1634426650022662_3330077380847861760_o.jpg', '#'),
+(2, 'Bạn bè', '50703016_2006719682961221_3670106943785009152_n.jpg', '#'),
+(3, 'Code', '000-Online-Code-Editors.png', '#'),
+(4, 'Thể thao', '131295505_2553480948285089_1597392856028183152_n.jpg', '#'),
+(5, 'Code', '92978774_131442888466194_6722416514584018944_n.jpg', '#');
+(6, 'Xe oto', 'gia-xe-bmw-i8-4-1779.jpg', '#');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resume`
+--
+
+CREATE TABLE `resume` (
+  `id` int(11) NOT NULL,
+  `category` varchar(250) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `year` varchar(250) NOT NULL,
+  `ogname` varchar(250) NOT NULL,
+  `workdesc` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `resume`
+--
+
+INSERT INTO `resume` (`id`, `category`, `title`, `year`, `ogname`, `workdesc`) VALUES
+(1, 'a', 'CLB Tin Học Sinh Viên', '2019-present', 'Đại Học Thủy Lợi', ''),
+(2, 'a', 'Thực tập', '2018-2020', 'FPT Company', ''),
+(3, 'a', 'Thực tập', '2020 - Present', 'VNIT Company', '');
+(4, 'b', 'CLB Tin Học', '2016-1018', 'THPT Trung Văn', 'Đội Phó');
+(5, 'b', 'Thành Viên', '2016 - 2018', 'CLB Tin Học Trường Cấp 3', 'Nhiều điều mới!');
+(6, 'b', 'Cấp 3', '2015-2018', 'THPT Trung Văn', 'Học Sinh');
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skills`
+--
+
+CREATE TABLE `skills` (
+  `id` int(11) NOT NULL,
+  `skill` varchar(250) NOT NULL,
+  `score` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `skills`
+--
+
+INSERT INTO `skills` (`id`, `skill`, `score`) VALUES
+(1, 'Java', '10'),
+(2, 'Swift', '66'),
+(3, 'IOS', '20'),
+(4, 'HTML', '91'),
+(5, 'PS6', '34'),
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userinfo`
+--
+
+CREATE TABLE `userinfo` (
+  `id` int(11) NOT NULL,
+  `userq` varchar(250) NOT NULL,
+  `userv` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `userinfo`
+--
+
+INSERT INTO `userinfo` (`id`, `userq`, `userv`) VALUES
+(1, 'Birthday', '06 Sept,2000'),
+(2, 'Age', '21'),
+(3, 'Website', 'www.kocoweb.com'),
+(4, 'Degree', 'Mỗ Lao,Hà Đông'),
+(5, 'Mobile', '0356342770'),
+(6, 'Email', 'nts6920@gmail.com'),
+(7, 'City', 'Hà Nội');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aboutus_setup`
+--
+ALTER TABLE `aboutus_setup`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admin_users`
+--
+ALTER TABLE `admin_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic_setup`
+--
+ALTER TABLE `basic_setup`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `personal_setup`
+--
+ALTER TABLE `personal_setup`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `portfolio`
+--
+ALTER TABLE `portfolio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `resume`
+--
+ALTER TABLE `resume`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `skills`
+--
+ALTER TABLE `skills`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userinfo`
+--
+ALTER TABLE `userinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `aboutus_setup`
+--
+ALTER TABLE `aboutus_setup`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `admin_users`
+--
+ALTER TABLE `admin_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `basic_setup`
+--
+ALTER TABLE `basic_setup`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `personal_setup`
+--
+ALTER TABLE `personal_setup`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `portfolio`
+--
+ALTER TABLE `portfolio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `resume`
+--
+ALTER TABLE `resume`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `skills`
+--
+ALTER TABLE `skills`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `userinfo`
+--
+ALTER TABLE `userinfo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+
